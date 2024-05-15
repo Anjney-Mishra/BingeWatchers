e78f515752cf5084423b66ff806cbc1d842b1703
AnjneyMishraTesting <anjneymishra21@gmail.com>
2024-05-15 20:44:23 +0530

    Fixed AUTH

---
+++ constants.js
@@ -0,0 +1,9 @@
+export const API_OPTIONS = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: import.meta.env.VITE_AUTH
+    }
+  };
+
+  export const IMG_CDN = "https://image.tmdb.org/t/p/w780/"
